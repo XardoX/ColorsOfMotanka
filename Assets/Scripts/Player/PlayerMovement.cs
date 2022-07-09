03dbd3caa0d59898bf502d5593dfaa493d2d34e1
Belfaust <46440980+Belfaust@users.noreply.github.com>
2022-07-09 13:11:13 +0200

    Resolved Changes

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Animator _animator;
     [SerializeField]
     float _moveInput;
     [SerializeField]
@@ -17,22 +18,47 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     float _jumpForce = 50;
     bool _isOnTheFloor = true;
-
+    float _horizontalMove;
+    
+    [SerializeField]
+    SpriteRenderer _spriteRenderer;
+    bool _flippedToRight = true;
     private Interactables currentNpc;
     
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponentInChildren<Animator>();
+        // _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
+    private void Update()
+    {
+        _horizontalMove = _currentSpeed * _moveInput;
+    }
+    
     void FixedUpdate()
     {
-        _rb.velocity = new Vector2(_currentSpeed * _moveInput, _rb.velocity.y);
+        _rb.velocity = new Vector2(_horizontalMove, _rb.velocity.y);
+        _animator.SetFloat("speed", Mathf.Abs( _horizontalMove));
+    }
+
+    void Flip()
+    {
+        if (_moveInput < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        else if(_moveInput > 0)
+        {
+            _spriteRenderer.flipX = false; 
+        }
     }
 
     void OnJump(InputValue value)
     {
         if(_isOnTheFloor) _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        _animator.SetBool("jump",true);
     }
     
     void OnInteract(InputValue value) => currentNpc?.OnInteract();
@@ -50,6 +76,7 @@ public class PlayerMovement : MonoBehaviour
         {
             currentMoveTween =  DOTween.To(() => _currentSpeed, x => _currentSpeed = x, 0, 1).SetEase(Ease.OutCirc);
         }
+        Flip();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -57,6 +84,7 @@ public class PlayerMovement : MonoBehaviour
         if(collision.collider.tag=="Platform")
         {
             _isOnTheFloor = true;
+            _animator.SetBool("jump", false);
         }
     }
 
